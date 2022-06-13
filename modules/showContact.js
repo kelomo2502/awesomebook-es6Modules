d69a2d926e0a51db050198763f3bec08e42bcaf9
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-06-13 08:57:56 +0100

    Add modules containing luxon.js,See.js,ShowBookList.js,ShowContact.js, Showform.js

---
+++ modules/showContact.js
@@ -0,0 +1,7 @@
+const showContact = () => {
+  document.getElementById('id1').style.display = 'none';
+  document.getElementById('id2').style.display = 'none';
+  document.getElementById('id3').style.display = 'block';
+};
+
+export default showContact;
---
+++ modules/showForm.js
@@ -0,0 +1,6 @@
+const showForm = () => {
+  document.getElementById('id1').style.display = 'none';
+  document.getElementById('id2').style.display = 'block';
+  document.getElementById('id3').style.display = 'none';
+};
+export default showForm;
